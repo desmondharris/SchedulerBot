fe891a71a35e9e1ab46f494602ae1a64fa6c8708
Desmond Harris <desmondktharris@gmail.com>
2024-02-26 23:09:34 -0500

    add sql, remove redundant files and imports

--- src/PersistentBot.py
+++ src/PersistentBot.py
@@ -4,28 +4,24 @@ It will be able to create events, commands, and tasks that will be persistent ac
 """
 import logging
 
-import telegram
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
     CommandHandler,
     Defaults,
-    ConversationHandler,
-    CallbackQueryHandler,
     MessageHandler,
     filters,
 )
 
+import mysql.connector
 
-import schedule
 import datetime, pytz
 
-import requests, json
-import os, sys
+import json
 
-from Calendar import Event
 from Keys import TELEGRAM_API_KEY, TELEGRAM_USER_ID, WEATHER_API_KEY
+from Keys import MYSQL_USER, MYSQL_PASSWORD
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -34,10 +30,18 @@ logging.basicConfig(
 # Turn off logging for HTTP POST requests
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
+
 class PersistentBot:
     def __init__(self):
         # Create bot
         self.app = ApplicationBuilder().token(TELEGRAM_API_KEY).defaults(Defaults(tzinfo=pytz.timezone("US/Eastern"))).build()
+        # Dummy attribute for sql connector
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database='telegram'
+        )
 
         # Add and configure handlers
         self.app.add_handler(CommandHandler("start", self.launch_web_ui))
@@ -50,7 +54,7 @@ class PersistentBot:
         # display our web-app!
         kb = [
             [KeyboardButton(
-                "Show me my Web-App!",
+                "Go to bot portal",
                 web_app=WebAppInfo("https://related-currently-maggot.ngrok-free.app/")
             )]
         ]
@@ -63,11 +67,9 @@ class PersistentBot:
         split_data = data.split('~')
         match split_data[0]:
             case "NONRECURRINGEVENT":
-                self.create_nr_event(split_data[1], datetime.datetime.strptime(' '.join([split_data[2], split_data[3]]), "%Y-%m-%d %H:%M"))
+                self.create_nr_event(context.job.data["chat_id"], split_data[1], datetime.datetime.strptime(' '.join([split_data[2], split_data[3]]), "%Y-%m-%d %H:%M"))
 
-        print(data.split('~'))
-
-    def create_nr_event(self, name: str, event_time: datetime.datetime):
+    def create_nr_event(self, chat_id, name: str, event_time: datetime.datetime):
         # Send message at event time
         self.app.job_queue.run_once(self.event_now, event_time, data={
             'name': name,
@@ -82,6 +84,9 @@ class PersistentBot:
         self.event_set_reminder(name, event_time, days=0, hours=4, minutes=0)
         self.event_set_reminder(name, event_time, days=1, hours=0, minutes=0)
         self.event_set_reminder(name, event_time, days=5, hours=0, minutes=0)
+
+        # Add event to events table
+        # self.db_insert("events", {"name": name, "time": event_time, "user": chat_id})
 
     async def event_now(self, context: ContextTypes.DEFAULT_TYPE):
         await self.app.bot.send_message(context.job.data['chat_id'], f"Event {context.job.data['name']} is happening now!")
@@ -99,7 +104,38 @@ class PersistentBot:
     async def event_send_reminder(self, context: ContextTypes.DEFAULT_TYPE):
         await self.app.bot.send_message(context.job.data['chat_id'], f"REMINDER: {context.job.data['name']} at {context.job.data['time']}")
 
+    def db_insert(self, table, data: dict):
+        """
+        table: str
+        data: dict
+        data is a dict that will contain the data to be inserted into the table
+        """
+        cursor = self.conn.cursor()
+        # Insert data into table
+        if table == "users":
+            query = "INSERT INTO users (chatid) VALUES (%s)"
+
+            # verify that data is an integer
+            if type(data["chatid"]) is not int:
+                raise ValueError(f"Table 'users' cannot have non integer chatid (got type {type(data['chatid'])})")
+
+            values = (data['chatid'],)
+            cursor.execute(query, values)
+            self.conn.commit()
+            cursor.close()
+
+        elif table == "events":
+            query = "INSERT INTO events(user, name, datetime) VALUES (%s, %s, %s)"
+
+            values = (data['name'], data['datetime'], data['user'])
+            cursor.execute(query, values)
+            self.conn.commit()
+            cursor.close()
+
+
+
 
 
 b = PersistentBot()
+b.db_insert("users", {"chatid": 1710495555})
 b.start_bot()
