7d66653b4fc703055e34e5e8d3ed54da3d8bd120
Desmond Harris <desmondktharris@gmail.com>
2024-02-27 00:10:45 -0500

    add events INSERT statements, fix checking if user exists

--- src/PersistentBot.py
+++ src/PersistentBot.py
@@ -51,11 +51,24 @@ class PersistentBot:
         self.app.run_polling()
 
     async def launch_web_ui(self, update: Update, callback: ContextTypes.DEFAULT_TYPE):
+
+        # Add user to database
+        cursor = self.conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM users WHERE chatid=%s", (update.message.chat_id,))
+        user_exists = cursor.fetchone()
+
+        if not user_exists:
+            query = "INSERT INTO users (chatid) VALUES (%s) ON DUPLICATE KEY UPDATE chatid=chatid;"
+            values = (update.message.chat_id,)
+            cursor.execute(query, values)
+        self.conn.commit()
+        cursor.close()
+
         # display our web-app!
         kb = [
             [KeyboardButton(
                 "Go to bot portal",
-                web_app=WebAppInfo("https://related-currently-maggot.ngrok-free.app/")
+                web_app=WebAppInfo("https://related-currently-maggot.ngrok-free.app")
             )]
         ]
         await update.message.reply_text("Let's do this...", reply_markup=ReplyKeyboardMarkup(kb))
@@ -67,38 +80,39 @@ class PersistentBot:
         split_data = data.split('~')
         match split_data[0]:
             case "NONRECURRINGEVENT":
-                self.create_nr_event(context.job.data["chat_id"], split_data[1], datetime.datetime.strptime(' '.join([split_data[2], split_data[3]]), "%Y-%m-%d %H:%M"))
+                                                                 #name           datetime (YYYY-MM-DD HH:MM)
+                self.create_nr_event(update.message.chat_id, split_data[1], datetime.datetime.strptime(' '.join([split_data[2], split_data[3]]), "%Y-%m-%d %H:%M"))
 
     def create_nr_event(self, chat_id, name: str, event_time: datetime.datetime):
         # Send message at event time
         self.app.job_queue.run_once(self.event_now, event_time, data={
             'name': name,
             'time': event_time,
-            'chat_id': TELEGRAM_USER_ID
+            'chat_id': chat_id
         })
         # Set reminders
         # NOTE: apscheduler automatically handles events that are in the past, this could
         # cause issues in the future?
-        self.event_set_reminder(name, event_time, days=0, hours=0, minutes=15)
-        self.event_set_reminder(name, event_time, days=0, hours=1, minutes=0)
-        self.event_set_reminder(name, event_time, days=0, hours=4, minutes=0)
-        self.event_set_reminder(name, event_time, days=1, hours=0, minutes=0)
-        self.event_set_reminder(name, event_time, days=5, hours=0, minutes=0)
+        self.event_set_reminder(chat_id, name, event_time, days=0, hours=0, minutes=15)
+        self.event_set_reminder(chat_id, name, event_time, days=0, hours=1, minutes=0)
+        self.event_set_reminder(chat_id, name, event_time, days=0, hours=4, minutes=0)
+        self.event_set_reminder(chat_id, name, event_time, days=1, hours=0, minutes=0)
+        self.event_set_reminder(chat_id, name, event_time, days=5, hours=0, minutes=0)
 
         # Add event to events table
-        # self.db_insert("events", {"name": name, "time": event_time, "user": chat_id})
+        self.db_insert("events", {"name": name, "datetime": event_time, "user": chat_id})
 
     async def event_now(self, context: ContextTypes.DEFAULT_TYPE):
         await self.app.bot.send_message(context.job.data['chat_id'], f"Event {context.job.data['name']} is happening now!")
 
-    def event_set_reminder(self, name: str, event_time: datetime.datetime, days=0, hours=0, minutes=0):
+    def event_set_reminder(self, chat_id, name: str, event_time: datetime.datetime, days=0, hours=0, minutes=0):
         reminder_time = event_time - datetime.timedelta(days=days, hours=hours, minutes=minutes)
         if reminder_time < datetime.datetime.now():
             return
         self.app.job_queue.run_once(self.event_send_reminder, reminder_time, data={
             'name': name,
             'time': event_time,
-            'chat_id': TELEGRAM_USER_ID
+            'chat_id': chat_id
         })
 
     async def event_send_reminder(self, context: ContextTypes.DEFAULT_TYPE):
@@ -110,7 +124,7 @@ class PersistentBot:
         data: dict
         data is a dict that will contain the data to be inserted into the table
         """
-        cursor = self.conn.cursor()
+        cursor = self.conn.cursor(buffered=True)
         # Insert data into table
         if table == "users":
             query = "INSERT INTO users (chatid) VALUES (%s)"
@@ -126,8 +140,7 @@ class PersistentBot:
 
         elif table == "events":
             query = "INSERT INTO events(user, name, datetime) VALUES (%s, %s, %s)"
-
-            values = (data['name'], data['datetime'], data['user'])
+            values = (data['user'], data['name'], data['datetime'],)
             cursor.execute(query, values)
             self.conn.commit()
             cursor.close()
@@ -137,5 +150,4 @@ class PersistentBot:
 
 
 b = PersistentBot()
-b.db_insert("users", {"chatid": 1710495555})
 b.start_bot()
