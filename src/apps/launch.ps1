78cf64127a70b8d4f55d39552ac1fe8cb4868245
Desmond Harris <desmondktharris@gmail.com>
2024-03-02 22:38:47 -0500

    fix typo

--- src/apps/launch.ps1
+++ src/apps/launch.ps1
@@ -1,2 +1,3 @@
-Start-Process powershell -ArgumentList  "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps.; python -m http.server 80"
-Start-Process powershell -ArgumentList "-noexit", "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps; .\ngrok http --domain=related-currently-maggot.ngrok-free.app 80"
+Start-Process powershell -ArgumentList  "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps; python -m http.server 80"
+
+Start-Process powershell -ArgumentList "-noexit", "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps; .\ngrok http --domain=regularly-unbiased-stud.ngrok-free.app 80"
