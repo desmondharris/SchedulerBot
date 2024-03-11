614d2c4dbace977492355098e4622de45673a3c0
Desmond Harris <desmondktharris@gmail.com>
2024-03-10 23:47:30 -0400

    inital commit

---
+++ scripts/botLaunch.ps1
@@ -0,0 +1,6 @@
+$scripts = $PSScriptRoot
+cd $scripts
+
+Start-Process powershell -ArgumentList "-noexit", ".\ngrok http --domain=regularly-unbiased-stud.ngrok-free.app 80"
+Start-Process powershell -ArgumentList "-noexit", "cd .\..\apps; python -m http.server 80"
+Start-Process powershell -ArgumentList "-noexit", ".\..\venv\Scripts\Activate; python .\..\src\PersistentBot.py"
---
+++ scripts/waLaunch.ps1
@@ -0,0 +1,5 @@
+$scripts = $PSScriptRoot
+cd $scripts
+
+Start-Process powershell -ArgumentList "-noexit", ".\ngrok http --domain=regularly-unbiased-stud.ngrok-free.app 80"
+Start-Process powershell -ArgumentList "-noexit", "cd .\..\apps; python -m http.server 80"
