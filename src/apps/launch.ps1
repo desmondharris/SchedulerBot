41f2353dd84ed065de06741ef9f86a2e1dddead2
Desmond Harris <desmondktharris@gmail.com>
2024-03-15 12:27:54 -0400

    delete old files

--- src/apps/launch.ps1
+++
@@ -1,4 +0,0 @@
-Start-Process powershell -ArgumentList "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps; python -m http.server 80"
-
-Start-Process powershell -ArgumentList "cd C:\Users\desmo\PycharmProjects\Scheduler\src\apps; .\ngrok http --domain=regularly-unbiased-stud.ngrok-free.app 80"
-Start-Process powershell -ArgumentList "-noexit", "cd C:\Users\desmo\PycharmProjects\Scheduler\src; .\..\venv\Scripts\Activate; python PersistentBot.py"
