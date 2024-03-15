613361565626022c916bddafb43c41905ccea79e
Desmond Harris <desmondktharris@gmail.com>
2024-03-15 18:36:43 -0400

    change python to run at module level

--- scripts/botLaunch.ps1
+++ scripts/botLaunch.ps1
@@ -3,4 +3,4 @@ cd $scripts
 
 Start-Process powershell -ArgumentList "-noexit", ".\ngrok http --domain=regularly-unbiased-stud.ngrok-free.app 80"
 Start-Process powershell -ArgumentList "-noexit", "cd .\..\apps; python -m http.server 80"
-Start-Process powershell -ArgumentList "-noexit", ".\..\venv\Scripts\Activate; python .\..\src\PersistentBot.py 0"
+Start-Process powershell -ArgumentList "-noexit", ".\..\venv\Scripts\Activate; cd ..; python -m src.PersistentBot 0"
