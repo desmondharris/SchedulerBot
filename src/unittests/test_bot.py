5dc510911fceabf1e2a9a6d32ef4ec7ea1b8c107
Altoid <desmondktharris@gmail.com>
2024-04-13 10:59:14 -0400

    add new unit tests with telethon

--- src/unittests/test_bot.py
+++ src/unittests/test_bot.py
@@ -6,148 +6,34 @@ from telegram.ext import ContextTypes, CallbackContext, ConversationHandler
 
 import datetime
 
-import sys
-
-sys.path.insert(0, '.\\')
 from src.PersistentBot import PersistentBot, GETZIP
-from src.BotSQL import BotSQL
+from src.BotSQL import *
+from src.unittests.BotTester import Tester
+
+CLI = Tester()
 
 
 @pytest.mark.asyncio
-async def test_start_command_response(mock_send_message, mock_db_insert, mock_exec):
-    # Create an instance of your bot
-    bot_instance = PersistentBot()
+async def test_main():
+    async def test_zip_convo():
+        await CLI.send_msg("/zip")
+        resp = await CLI.fetch_responses(1)
+        assert resp[0].message == "Enter your ZIP code:"
 
-    # Simulate an update object msg="/start"
-    update = Update(
-        update_id=1,
-        message=Message(
-            message_id=1,
-            date=datetime.datetime.now(),
-            chat=Chat(id=12345, type='private'),
-            text='/start',
-            entities=[MessageEntity(type='bot_command', offset=0, length=6)],
-            from_user=User(id=12345, is_bot=False, first_name='Test User'),
-        )
-    )
+        await CLI.send_msg("55555")
+        resp = await CLI.fetch_responses(1)
+        assert resp[0].message == "Your zip code has been added!"
 
-    context = ContextTypes.DEFAULT_TYPE(bot_instance.app)
+    await test_zip_convo()
 
-    # Mock sending message
-    await bot_instance.start(update, context)
-    mock_send_message.assert_awaited_with(12345, text="Welcome to the bot! To get weather data, I need to know your ZIP code. To enter it, type /zip at anytime. This is NOT required.")
+    async def test_add_revent():
+        await CLI.send_msg("/new")
+        resp = await CLI.fetch_responses(1)
+        assert resp[0].message == "Launching portal..."
+        # Send mock request
 
+    await test_add_revent()
 
-@pytest.mark.asyncio
-async def test_zip_handler(mock_reply_text, mock_send_message, mock_exec):
-    bot = PersistentBot()
-
-    update = Update(
-        update_id=1,
-        message=Message(
-            message_id=1,
-            date=datetime.datetime.now(),
-            chat=Chat(id=12345, type='private'),
-            text='/zip',
-            entities=[MessageEntity(type='bot_command', offset=0, length=4)],
-            from_user=User(id=12345, is_bot=False, first_name='Test User'),
-        )
-    )
-    # Example of an update object from the webapp
-    """
-    Update(message=Message(channel_chat_created=False, chat=Chat(first_name='Desmond', id=1710495555, last_name='Harris',
-     type=<ChatType.PRIVATE>, username='desmondktharris'), date=datetime.datetime(2024, 3, 15, 18, 53, 7, tzinfo=<DstTzInfo 
-     'US/Eastern' EDT-1 day, 20:00:00 DST>), delete_chat_photo=False, from_user=User(first_name='Desmond', id=1710495555,
-      is_bot=False, language_code='en', last_name='Harris', username='desmondktharris'), group_chat_created=False, 
-      message_id=933, supergroup_chat_created=False, web_app_data=WebAppData(button_text='Go to bot portal', 
-      data='{"type":"NONRECURRINGEVENT","eventName":"fsdf","eventDate":"2024-03-15","eventTime":"18:53",
-      "reminders":["1-HOURS"]}')), update_id=166519222)
-    """
-    context = ContextTypes.DEFAULT_TYPE(bot.app)
-    state = await bot.zip(update, context)
-    assert state == GETZIP
-
-    update = Update(
-        update_id=2,
-        message=Message(
-            message_id=2,
-            date=datetime.datetime.now(),
-            chat=Chat(id=12345, type='private'),
-            text='12345',
-            from_user=User(id=12345, is_bot=False, first_name='Test User'),
-        )
-    )
-    state = await bot.get_zip(update, context)
-    # check if send message was asserted with zip code saved or zip code not saved
-    mock_reply_text.assert_awaited()
-    assert state == ConversationHandler.END
-
-
-@pytest.mark.asyncio
-async def test_nonrecurring_event(mock_send_message, mock_db_insert, mock_run_once, mock_set_reminder, mock_exec, mock_insert_zip):
-    bot = PersistentBot()
-    # Create a generic version of this update
-    """
-    Update(
-    message=Message(channel_chat_created=False,
-    chat=Chat(first_name='Desmond', id=1710495555, last_name='Harris', type=<ChatType.PRIVATE>, username='desmondktharris'), 
-    date=datetime.datetime(2024, 3, 15, 18, 53, 7, tzinfo=<DstTzInfo 'US/Eastern' EDT-1 day, 20:00:00 DST>), 
-    delete_chat_photo=False, 
-    from_user=User(first_name='Desmond', id=1710495555, is_bot=False, language_code='en', last_name='Harris', username='desmondktharris'), 
-    group_chat_created=False, 
-    message_id=933,
-    supergroup_chat_created=False,
-    web_app_data=WebAppData(button_text='Go to bot portal', 
-      data='{"type":"NONRECURRINGEVENT","eventName":"fsdf","eventDate":"2024-03-15","eventTime":"18:53",
-      "reminders":["1-HOURS"]}')), update_id=166519222)
-    """
-    # convert this to a dictionary
-    eventType = "NONRECURRINGEVENT"
-    event_name = "Unit Test Event"
-    event_date = "2024-03-15"
-    event_time = "18:53"
-    reminders = ["5-MINUTES"]
-
-    update = Update(
-        message=Message(
-            channel_chat_created=False,
-            chat=Chat(first_name='Desmond', id=172345, last_name='Harris', type='private',
-                      username='desmondktharris'),
-            date=datetime.datetime.now(),
-            delete_chat_photo=False,
-            from_user=User(first_name='John', id=172345, is_bot=False, language_code='en'),
-            group_chat_created=False,
-            message_id=933,
-            supergroup_chat_created=False,
-            web_app_data=WebAppData(button_text='Go to bot portal',
-                                    data=f'{{"type":"{eventType}","eventName":"{event_name}","eventDate":"{event_date}","eventTime":"{event_time}","reminders":["5-MINUTES"]}}')),
-        update_id=166519222
-    )
-    context = ContextTypes.DEFAULT_TYPE(bot.app)
-    await bot.web_app_data(update, context)
-    mock_send_message.assert_awaited_with(172345, text="Event added!")
-    mock_run_once.assert_called_once()
-
-
-# @pytest.mark.asyncio
-# async def test_recurring_event(mock_db_insert):
-#     pass
-#     bot = PersistentBot()
-#
-#     # Create update object with recurring event
-#     # update = ...
-#
-#     # Intitialize event arguments
-#     #name = ...
-#
-#     # Call bot.web_app_data
-#
-#     #bot.create_r_event.assert_called_with()
-#
-#     #bot.mock_db_insert.assert_called_with()
-#
-#     #assert bot.r_event_set_reminder was called with all appropriate sets of args
-    
 
 
 
