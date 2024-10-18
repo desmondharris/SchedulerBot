1d45716fc50a4bc5aca900ba29b4fb15801a0bcc
Desmond Harris <dkharrisdev@gmail.com>
2024-10-18 16:07:43 +0000

    initial commit

---
+++ src/BotRefactor.py
@@ -0,0 +1,7 @@
+"""
+- give up supporting multiple time zones for now
+- READ PYTB GitHub DOCS! CODE LOGIC EXPLAINED THERE
+- no classes in the bot, only for peewee models
+    ex: https://github.com/codewarnab/akinatorbot/blob/main/__main__.py
+- remove webapp entirely?
+"""
