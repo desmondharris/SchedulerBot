dc066750de437448381a1c6d446fe70ac5449b40
Desmond Harris <dkharr05@louisville.edu>
2024-10-21 16:56:05 -0400

    add reminder callbackqueryhandler

--- src/BotRefactor.py
+++ src/BotRefactor.py
@@ -9,7 +9,8 @@ import peewee
 
 import telegram
 from telegram import ForceReply, Update, Message, MessageEntity, Bot
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler, \
+    CallbackQueryHandler
 
 from .BotSQL import Chat, RecurringEvent, NonRecurringEvent
 from .MessageOptions import *
@@ -41,7 +42,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Message:
         return await update.get_bot().send_message(update.message.chat_id, START_HELP_MESSAGE)
 
 
-async def onetime_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def onetime_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Message:
     """
     Entry point for creating a one-time event
     @param update: update.message != none
@@ -60,24 +61,44 @@ async def onetime_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         _, weekday, time, event_name = info.split(maxsplit=3)
         logger.debug((weekday, time, event_name))
         res = await update.message.reply_text(WEEKDAY_INLINE_TEXT, reply_markup=WEEKDAY_INLINE_KB)
+        NonRecurringEvent.create(
+            user=update.effective_chat.id,
+            name=event_name,
+            date=datetime.date.today(),
+            time=datetime.datetime.strptime(time, "%H%M").time(),
+            reminders="",
+            reminder_open = True
+        )
         return res
     else:
         logger.warning(f"User {update.effective_user} in chat {update.effective_chat} attempted invalid ote")
         await update.message.reply_text("Please input a one-time event in one of the proper formats!")
 
 
+async def onetime_reminder_callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Callback handler for one-time event reminders
+    @param update: update.callback_query != none
+    @param context: unused
+    @return: none
+    """
+    query = update.callback_query
+    await query.answer()
+    length, unit = query.data.split("-")
+    length = int(length)
+    res = await query.message.reply_text(f"Set {length} {unit[:len(unit)-1]} reminder.")
+    return res
 
-"""
-Replace web app with:
-monday 1700 Brown Fellow meeting
-bot replies with checklist of options
-"""
+
+def add_handlers(application: Application) -> None:
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("ot", onetime_start))
+    application.add_handler(CallbackQueryHandler(onetime_reminder_callback_handler))
 
 
 def main() -> None:
     application = Application.builder().token(os.getenv("TELEGRAM_API_KEY")).build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("ot", onetime_start))
+    add_handlers(application)
     application.run_polling()
 
 
