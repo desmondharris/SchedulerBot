8f96b7965c12f684116b6c508d0542637b84ef04
Desmond Harris <dkharr05@louisville.edu>
2024-10-21 16:57:22 -0400

    remove spelling mistake

--- src/MessageOptions.py
+++ src/MessageOptions.py
@@ -6,7 +6,7 @@ WEEKDAY_INLINE_KB = InlineKeyboardMarkup([
     [InlineKeyboardButton("5 Minutes", callback_data="5-minutes"),
      InlineKeyboardButton("15 Minutes", callback_data="15-minutes"),
      InlineKeyboardButton("30 Minutes", callback_data="30-minutes")],
-    [InlineKeyboardButton("1 Hour", callback_data="1-hour"),
+    [InlineKeyboardButton("1 Hour", callback_data="1-hours"),
         InlineKeyboardButton("2 Hours", callback_data="2-hours"),
         InlineKeyboardButton("1 day", callback_data="4-hours")],
 ])
